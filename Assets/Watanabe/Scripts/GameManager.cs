794420c72fe5383b5369fba3aa3dfb899c1c8848
AkiraWatanabe23 <a0323w@gmail.com>
2023-09-26 12:17:20 +0900

    [update] カウントダウンシステム

---
+++ Assets/Watanabe/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Tooltip("何秒のカウントダウンで始めるか")]
+    [SerializeField]
+    private int _countDownTime = 0;
+    [SerializeField]
+    private UIManager _uiManager = new();
+
+    private bool _isGameStart = false;
+
+    public bool IsGameStart => _isGameStart;
+
+    private void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    private IEnumerator CountDown()
+    {
+        for (int i = _countDownTime; i >= 0; i--)
+        {
+            if (i == 0)
+            {
+                _uiManager.CountDownText.text = "すたーと！！";
+                _isGameStart = true;
+                yield return new WaitForSeconds(1f);
+                _uiManager.CountDownText.text = "";
+                yield break;
+            }
+
+            _uiManager.CountDownText.text = i.ToString();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
---
+++ Assets/Watanabe/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[Serializable]
+/// <summary> インゲームのUI管理クラス </summary>
+public class UIManager
+{
+    [SerializeField]
+    private Text _countDownText = default;
+    [Tooltip("ゲームの進行時間を表示するText")]
+    [SerializeField]
+    private Text _playTimerText = default;
+
+    public Text CountDownText { get => _countDownText; set => _countDownText = value; }
+}
